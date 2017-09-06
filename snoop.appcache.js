df51a2ddd46bddf32bbf9659b4b7a61ee3dd94c7
Samir Dash <sdash@redhat.com>
2017-09-06 10:19:34 +0530

    Update snoop.appcache.js

--- snoop.appcache.js
+++ snoop.appcache.js
@@ -1,13 +1,13 @@
 CACHE MANIFEST
       
-# Buddy Appcache manifest file
+# Snoop Appcache manifest file
 
 CACHE:
 /css/screen.css
 /css/offline.css
 /js/screen.js
 /img/logo.png
-http://example.com/css/styles.css
+http://redhat.com/css/styles.css
 
 FALLBACK:
 / /offline.html
